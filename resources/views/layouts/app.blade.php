cf8b011757102134ffa9effc402920db07724d87
Andika Eka Pratomo <andikaekapratomo2005@gmail.com>
2026-07-16 10:01:24 +0700

    mengganti footer

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>@yield('title', 'SiTernak') - Sistem Manajemen Ternak</title>
+    <title>@yield('title', 'SiTernak') - Sistem Inventory Ternak</title>
 
     {{-- Skydash CSS --}}
     <link rel="stylesheet" href="{{ asset('vendors/feather/feather.css') }}">
--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -1,7 +1,7 @@
 <footer class="footer">
     <div class="d-sm-flex justify-content-center">
         <span class="text-muted text-center d-block">
-            &copy; {{ date('Y') }} <strong>Sumber Makmur</strong> — Sistem Manajemen Ternak
+            &copy; {{ date('Y') }} <strong>Sumber Makmur</strong> — Sistem Inventory Ternak
         </span>
     </div>
 </footer>
